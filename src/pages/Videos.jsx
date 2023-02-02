6c968e051e84c5a88c5ff2f2abebfd16a971913b
hyukee0205 <hyukee0205@gmail.com>
2023-02-02 18:48:55 +0900

    0202 오류수정

---
+++ src/pages/Videos.jsx
@@ -0,0 +1,25 @@
+import React, { useLayoutEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import VideoCard from '../components/VideoCard';
+
+export default function Videos() {
+  const {keyword} = useParams();
+  const {isLoading, error, data: videos
+  } = useQuery(['videos', keyword], async () => {
+    return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
+    .then(res => res.json())
+    .then(data => data.items);
+  });
+
+  return(
+    <>
+      <div>Videos {keyword ? `🔍${keyword}` : '🔥' }</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong 😲</p>}
+      {videos && <ul>
+        {videos.map(video => <VideoCard key={video.id} video={video}/>)}
+        </ul>}
+    </>
+  )
+}
